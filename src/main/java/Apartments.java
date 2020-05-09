0a83b9b568949b8b2f0826ee6ba23a338ead6f9d
disel20 <rohivevgen@gmail.com>
2020-05-08 21:52:41 -0400

    Add Apartment Studio

--- src/main/java/Apartments.java
+++ src/main/java/Apartments.java
@@ -1,7 +1,16 @@
 public class Apartments extends Buildings {
-    private static Object String;
-
+    private int NumberRooms;
+    private String NumberOfApartment;
     public static void main(String[] args) {
-        String = "name";
+        String Name = "Apts";
+        //int NumberRooms;
+
+        Apartments Studio = new Apartments(0, "1A");
+        System.out.println("Hello!");
+        System.out.println("My apartment is: " + Studio.NumberOfApartment + " this is " + Studio.NumberRooms + " bedroom apartment");
+    }
+    private Apartments(int NumberRooms, String NumberOfApartment) {
+        this.NumberRooms = NumberRooms;
+        this.NumberOfApartment = NumberOfApartment;
     }
 }
--- src/main/java/Buildings.java
+++ src/main/java/Buildings.java
@@ -1,2 +1,3 @@
 abstract class Buildings {
+    public static Object String;
 }
